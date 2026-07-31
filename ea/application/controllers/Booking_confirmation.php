cc6634f2e5c317d598b6a7d372b30c744f8b5f54
Hillkim <hillkim@pm.me>
2026-07-31 14:28:17 +0300

    feat: add Outlook calendar button and ICS download endpoint to confirmation page

--- ea/application/controllers/Booking_confirmation.php
+++ ea/application/controllers/Booking_confirmation.php
@@ -55,6 +55,7 @@ class Booking_confirmation extends EA_Controller
         $appointment = $occurrences[0];
 
         $add_to_google_url = $this->google_sync->get_add_to_google_url($appointment['id']);
+        $add_to_outlook_url = $this->google_sync->get_add_to_outlook_url($appointment['id']);
 
         html_vars([
             'page_title' => lang('success'),
@@ -63,9 +64,41 @@ class Booking_confirmation extends EA_Controller
             'matomo_analytics_url' => setting('matomo_analytics_url'),
             'matomo_analytics_site_id' => setting('matomo_analytics_site_id'),
             'add_to_google_url' => $add_to_google_url,
+            'add_to_outlook_url' => $add_to_outlook_url,
             'display_add_to_google_calendar' => setting('display_add_to_google_calendar', '1'),
+            'display_add_to_outlook_calendar' => setting('display_add_to_outlook_calendar', '1'),
         ]);
 
         $this->load->view('pages/booking_confirmation');
+    }
+
+    /**
+     * Download the ICS file for an appointment.
+     *
+     * @param string $appointment_hash
+     *
+     * @throws Exception
+     */
+    public function ics(string $appointment_hash): void
+    {
+        $occurrences = $this->appointments_model->get(['hash' => $appointment_hash]);
+
+        if (empty($occurrences)) {
+            show_404();
+            return;
+        }
+
+        $appointment = $occurrences[0];
+        $service = $this->services_model->find($appointment['id_services']);
+        $provider = $this->providers_model->find($appointment['id_users_provider']);
+        $customer = $this->customers_model->find($appointment['id_users_customer']);
+
+        $this->load->library('ics_file');
+        $ics_stream = $this->ics_file->get_stream($appointment, $service, $provider, $customer);
+
+        $this->output
+            ->set_content_type('text/calendar; charset=utf-8')
+            ->set_header('Content-Disposition: attachment; filename="appointment.ics"')
+            ->set_output($ics_stream);
     }
 }
